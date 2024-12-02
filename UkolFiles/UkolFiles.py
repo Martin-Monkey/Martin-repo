51bc37e870033b8ce9974875880a821f5541fb29
Martin-Monkey <martin.sestak92@gmail.com>
2024-12-02 20:59:39 +0100

    task 1

--- UkolFiles/UkolFiles.py
+++ UkolFiles/UkolFiles.py
@@ -1,1 +1,20 @@
+class Files_compare:
+    def __init__(self, file1, file2):
+        self.file1 = file1
+        self.file2 = file2
 
+    def Compare(self):
+        with open(self.file1, 'r') as f1, open(self.file2, 'r') as f2:
+            lines1 = f1.readlines()
+            lines2 = f2.readlines()
+
+            for i, (line1, line2) in enumerate(zip(lines1, lines2)):
+                if line1 != line2:
+                    print(f"Neshodný řádek {i + 1}:")
+                    print(f"Soubor 1: {line1.strip()}")
+                    print(f"Soubor 2: {line2.strip()}")
+
+
+# Příklad volání:
+Comparator = Files_compare('file1.txt', 'file2.txt')
+Comparator.Compare()
