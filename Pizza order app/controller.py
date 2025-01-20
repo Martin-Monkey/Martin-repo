cb8d9eadb156362ac9a17c38d18dfaac84b23c35
Martin-Monkey <martin.sestak92@gmail.com>
2025-01-20 20:52:38 +0100

    controller update

--- Pizza order app/controller.py
+++ Pizza order app/controller.py
@@ -1,22 +1,34 @@
-from model import Pizza, Topping, Order, Sales, CashPayment
+# controller.py
+
+from model import Pizza, Topping, Order, Sales, CashPayment, CreditCardPayment, create_pizzas
+from view import UI
 
 class MainController:
     def __init__(self):
         self.sales = Sales()
         self.order = None
+        self.pizzas = create_pizzas()
 
     def create_order(self):
         self.order = Order()
-        pizza = Pizza("Margherita", "Medium", 8)
+        UI.show_pizzas(self.pizzas)
+        choice = int(input("Select pizza (1-5): ")) - 1
+        pizza = self.pizzas[choice]
         self.order.add_pizza(pizza)
-        topping = Topping("Olives", 1)
-        pizza.add_topping(topping)
+        UI.show_order_summary(self.order)
 
     def process_payment(self):
-        payment_method = CashPayment()  # Můžete změnit na CreditCardPayment pro jiný způsob platby
+        UI.show_payment_method()
+        method_choice = int(input("Choose payment method: "))
+        if method_choice == 1:
+            payment_method = CreditCardPayment()
+        elif method_choice == 2:
+            payment_method = CashPayment()
+        else:
+            print("Invalid option.")
+            return
         print(payment_method.pay(self.order.total))
         self.sales.record_sale(self.order.total)
 
     def show_sales(self):
         print(self.sales.show_sales())
-
