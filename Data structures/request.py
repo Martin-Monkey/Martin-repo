818a87e50f4c6b1c873ffd9a31395721a2517f24
Martin-Monkey <martin.sestak92@gmail.com>
2025-01-20 19:28:58 +0100

    request full

---
+++ Data structures/request.py
@@ -0,0 +1,24 @@
+import time
+import queue
+
+class Request:
+    def __init__(self, user, priority):
+        self.user = user
+        self.priority = priority
+        self.time = time.time()  # Čas odeslání požadavku
+
+    def __lt__(self, other):
+        # Tato metoda zajišťuje správné řazení požadavků ve frontě podle priority.
+        return self.priority < other.priority  # Nižší číslo = vyšší priorita
+
+    def __str__(self):
+        return f"Request(user={self.user}, priority={self.priority}, time={self.time})"
+
+
+class Client:
+    def __init__(self, name):
+        self.name = name
+
+    def create_request(self, priority):
+        # Vytvoření požadavku s určitou prioritou
+        return Request(self.name, priority)
