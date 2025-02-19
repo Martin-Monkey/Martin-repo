64cee44d54af182d76c12c8ea3a08210c0f44956
Martin-Monkey <martin.sestak92@gmail.com>
2025-02-19 21:21:11 +0100

    bla

--- cviceni/main.py
+++ cviceni/main.py
@@ -1,16 +1,61 @@
-# This is a sample Python script.
+import json
+import pickle
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# 1. Třída Student
+class Student:
+    def __init__(self, name, age, grades):
+        self.name = name
+        self.age = age
+        self.grades = grades
+
+    def __str__(self):
+        return f"Student: {self.name}, Age: {self.age}, Grades: {self.grades}"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# 2. Uložení objektu do souboru
+def save_to_json(student, filename):
+    with open(filename, 'w') as f:
+        json.dump(student.__dict__, f)
 
+def save_to_pickle(student, filename):
+    with open(filename, 'wb') as f:
+        pickle.dump(student, f)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# 3. Načtení objektu ze souboru
+def load_from_json(filename):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+        return Student(data['name'], data['age'], data['grades'])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def load_from_pickle(filename):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
+
+# Příklad použití
+student1 = Student("Martin Sestak", 20, [1, 2, 1, 3])
+
+# Uložení do souborů
+save_to_json(student1, "student.json")
+save_to_pickle(student1, "student.pkl")
+
+# Načtení z JSON a Pickle
+student_from_json = load_from_json("student.json")
+student_from_pickle = load_from_pickle("student.pkl")
+
+# Výpis obsahů
+print("Načtený student z JSON:")
+print(student_from_json)
+
+print("Načtený student z Pickle:")
+print(student_from_pickle)
+
+mport
+csv
+
+# Otevření CSV souboru
+with open('data.csv', mode='r', newline='', encoding='utf-8') as file:
+    reader = csv.reader(file)
+
+    # Přečte všechny řádky a vypíše je
+    for row in reader:
+        print(row)
